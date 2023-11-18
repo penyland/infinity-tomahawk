94173f3152724997b109deab0263c9244ec0363e
Peter Nylander <peter.nylander@gmail.com>
2023-11-18 16:04:01 +0100

    Creating .NET Aspire Dashboard feature module

---
+++ src/Infinity.Toolkit.FeatureModule.AspireDashboard/AspireDashboardViewModelService.cs
@@ -0,0 +1,23 @@
+﻿using Aspire.Dashboard.Model;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Infinity.Toolkit.FeatureModule.AspireDashboard;
+
+public record AspireDashboardViewModelService(string ApplicationName) : IDashboardViewModelService
+{
+    public AspireDashboardViewModelService(IWebHostEnvironment webHostEnvironment) : this(webHostEnvironment.ApplicationName)
+    {
+    }
+
+    public ValueTask<List<ContainerViewModel>> GetContainersAsync() => ValueTask.FromResult(new List<ContainerViewModel>());
+
+    public ValueTask<List<ExecutableViewModel>> GetExecutablesAsync() => ValueTask.FromResult(new List<ExecutableViewModel>());
+
+    public ValueTask<List<ProjectViewModel>> GetProjectsAsync() => ValueTask.FromResult(new List<ProjectViewModel>());
+
+    public IAsyncEnumerable<ResourceChanged<ContainerViewModel>> WatchContainersAsync(IEnumerable<NamespacedName>? existingContainers = null, CancellationToken cancellationToken = default) => AsyncEnumerable.Empty<ResourceChanged<ContainerViewModel>>();
+
+    public IAsyncEnumerable<ResourceChanged<ExecutableViewModel>> WatchExecutablesAsync(IEnumerable<NamespacedName>? existingExecutables = null, CancellationToken cancellationToken = default) => AsyncEnumerable.Empty<ResourceChanged<ExecutableViewModel>>();
+
+    public IAsyncEnumerable<ResourceChanged<ProjectViewModel>> WatchProjectsAsync(IEnumerable<NamespacedName>? existingProjects = null, CancellationToken cancellationToken = default) => AsyncEnumerable.Empty<ResourceChanged<ProjectViewModel>>();
+}
---
+++ src/Infinity.Toolkit.FeatureModule.AspireDashboard/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Aspire.Dashboard;
+using Aspire.Dashboard.Model;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Infinity.Toolkit.FeatureModule.AspireDashboard;
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddAspireDashboard(this IServiceCollection services)
+    {
+        services.AddHostedService((sp) =>
+        {
+            var application = new DashboardWebApplication(sp.GetRequiredService<ILogger<DashboardWebApplication>>(), configureServices =>
+            {
+                configureServices.AddSingleton<IDashboardViewModelService, AspireDashboardViewModelService>();
+            });
+
+            return application;
+        });
+
+        return services;
+    }
+}
