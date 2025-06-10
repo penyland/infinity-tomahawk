e237214916af3ec13b9d5c4346e70c62737b222c
Peter Nylander <peter.nylander@gmail.com>
2025-06-10 17:41:39 +0200

    Improvements to Result in Experimental

--- src/Infinity.Toolkit.Experimental/SuccessResult.cs
+++ src/Infinity.Toolkit.Experimental/SuccessResult.cs
@@ -1,23 +1,17 @@
 ﻿namespace Infinity.Toolkit.Experimental;
-#pragma warning restore SA1302 // Interface names should begin with I
-#pragma warning restore IDE1006 // Naming Styles
 
 public class SuccessResult : Result, Success
 {
-    public SuccessResult()
+    public SuccessResult() : base()
     {
-        Succeeded = true;
     }
 }
 
 public class SuccessResult<T> : Result<T>, Success
 {
     public SuccessResult()
-        : base(default(T)!) => Succeeded = true;
+        : base(default!, []) => Succeeded = true;
 
     public SuccessResult(T data)
-        : base(data)
-    {
-        Succeeded = true;
-    }
+        : base(data, []) => Succeeded = true;
 }
